911222ff991ae6b34c6a5e74a19aaee45c412cb4
isaac.garcia <isaac.garcia@softplan.com.br>
2024-02-23 00:28:55 -0300

    Configurações iniciais do aplicativo

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        ["module-resolver", {
+            "root": ["./src"],
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
+            alias: {
+                "@assets": "./src/assets",
+                "@components": "./src/components",
+                "@config": "./src/condig",
+                "@hooks": "./src/hooks",
+                "@routes": "./src/routes",
+                "@screens": "./src/screens",
+                "@services": "./src/services",
+                "@styles": "./src/styles",
+                "@utils": "./src/utils",
+            }
+        }
+        ]
+    ],
 };
